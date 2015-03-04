6ab8a8861f8072f3065418d676090a627df9bb7a
markhibberd <mark@hibberd.id.au>
2015-03-04 14:07:02 +1100

    Generic jsonProp & build.

---
+++ orphanarium-aeson/src/Orphanarium/Aeson.hs
@@ -0,0 +1,10 @@
+module Orphanarium.Aeson where
+
+import           Data.Aeson
+import           Data.Aeson.Types
+
+import           Test.QuickCheck
+
+jsonProp :: (ToJSON a, FromJSON a, Eq a, Show a) => a -> Property
+jsonProp a =
+  parseEither (parseJSON . toJSON) a === Right a
