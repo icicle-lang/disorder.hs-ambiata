408a56e407ec20e13eb13bc9391f23407357e1e3
Dom De Re <domdere@domdere.com>
2015-04-15 13:39:31 +1000

    Property to test the version in json Objects

--- orphanarium-aeson/test/Orphanarium/AesonTest.hs
+++ orphanarium-aeson/test/Orphanarium/AesonTest.hs
@@ -4,11 +4,18 @@ module Orphanarium.AesonTest where
 
 import           Orphanarium.Aeson
 
+import           Data.Aeson ( (.=), object, toJSON )
+import qualified Data.Text as T
+
 import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
 
 prop_jsonProp :: Int -> Property
 prop_jsonProp =
   jsonProp
+
+prop_jsonVersionProp :: T.Text -> Property
+prop_jsonVersionProp t = jsonVersionProp (toJSON t) $ object ["version" .= t]
 
 return []
 tests :: IO Bool
