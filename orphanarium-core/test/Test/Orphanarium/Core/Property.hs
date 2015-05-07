e8da0501a4d5d43c032222d87ad02a916b4a16b4
mth <mark@hibberd.id.au>
2015-05-07 21:46:28 +1000

    Formatting.

--- orphanarium-core/test/Test/Orphanarium/Core/Property.hs
+++ orphanarium-core/test/Test/Orphanarium/Core/Property.hs
@@ -17,15 +17,18 @@ prop_notEquals x y =
   (x /= y) ==> x =/= y
 
 prop_equalsXor :: (Arbitrary a, Show a, Eq a) => a -> a -> Property
-prop_equalsXor x y = (x === y) .^. (x =/= y)
+prop_equalsXor x y =
+  (x === y) .^. (x =/= y)
 
 prop_negEquals :: (Arbitrary a, Show a, Eq a) => a -> a -> Property
-prop_negEquals x y = (x =/= y) <=> neg (x === y)
+prop_negEquals x y =
+  (x =/= y) <=> neg (x === y)
 
 -- |
 -- @p .^. neg p@
 prop_negXor :: (Arbitrary a, Show a, Eq a) => a -> a -> Property
-prop_negXor x y = (x === y) .^. neg (x === y)
+prop_negXor x y =
+  (x === y) .^. neg (x === y)
 
 return []
 tests :: IO Bool
