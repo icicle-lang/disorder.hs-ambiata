a6adcb9a08cd0c0c13501298c6934d0b1b384b97
Eric Torreborre <etorreborre@yahoo.com>
2015-05-15 17:00:35 +1000

    Revert "added NaturalInt and PositiveInt. fixes #27"
    This reverts commit 7e0eaafa7267c8cf8a5cabf79f86887bfbcfea56.

--- disorder-core/test/test.hs
+++ disorder-core/test/test.hs
@@ -2,16 +2,12 @@ import           Disorder.Core.Main
 import qualified Test.Disorder.Core.Gen
 import qualified Test.Disorder.Core.Property
 import qualified Test.Disorder.Core.UniquePair
-import qualified Test.Disorder.Core.NaturalInt
 import qualified Test.Disorder.Core.OrdPair
-import qualified Test.Disorder.Core.PositiveInt
 
 main :: IO ()
 main = disorderMain [
     Test.Disorder.Core.Gen.tests
   , Test.Disorder.Core.Property.tests
   , Test.Disorder.Core.UniquePair.tests
-  , Test.Disorder.Core.NaturalInt.tests
   , Test.Disorder.Core.OrdPair.tests
-  , Test.Disorder.Core.PositiveInt.tests
   ]
