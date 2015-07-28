3e17cf8afa2d1cf89f2af996d28d5b0392ae06d6
Eduard Sergeev <eduard.sergeev@ambiata.com>
2015-07-29 00:51:40 +1000

    Refactor Cont tests and add more comments
    `pick` function is really handy for using 'Gen a' inside 'Transition'

--- disorder-fsm/test/Test/Disorder/FSM/Cont.hs
+++ disorder-fsm/test/Test/Disorder/FSM/Cont.hs
@@ -5,6 +5,7 @@ import qualified Data.List as L
 import           Data.Monoid
 import qualified Data.Map.Strict as M
 import           Data.Maybe
+import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.Cont
 import           System.IO
@@ -16,10 +17,12 @@ import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 
 
+-- | Environment of the model is just a root temporary directory
 newtype Environment = Environment {
     workingDirectory :: FilePath
   }
 
+-- | Model state: simple "file system"
 newtype FileSystem = FileSystem {
     fileSystemFiles :: M.Map FilePath File
   }
@@ -28,7 +31,9 @@ mkFileSystem :: FileSystem
 mkFileSystem = FileSystem M.empty
 
 data File = File {
+    -- | Nothing if file is closed
     fileHandle :: Maybe Handle
+    -- | Mirrors the actual file content
   , fileContent :: [String]
   } deriving (Show )
 
@@ -40,6 +45,8 @@ instance Show FileSystem where
       showInfo (File (Just _) c) = "opened, " <> showContent c
       showInfo (File Nothing c) = "closed, " <> showContent c
       showContent c = show (L.length c) <> " line(s)"
+
+-- * 'ContT' version of "bracket" system fuctions
 
 withSystemTempDirectoryCont :: String -> ContT a IO FilePath
 withSystemTempDirectoryCont = ContT . withSystemTempDirectory
@@ -53,10 +60,14 @@ withTempFileCont d t = ContT $ \fc -> withTempFile d t (curry fc)
 withFileCont :: FilePath -> IOMode -> ContT a IO Handle
 withFileCont fp = ContT . withFile fp
 
-
+-- | FSM 'Transition' state
 type FileTransition = Transition Environment FileSystem (ContT Property IO)
 
+-- * Model transitions
 
+-- | Creates new empty file inside 'withTempFile' continuation
+--   so the file is deleted after tests
+--   file stays open after this step
 genCreateFile :: Gen FileTransition
 genCreateFile = do
   return $ mkTransition "create file" `goto` do
@@ -66,67 +77,78 @@ genCreateFile = do
         fileSystemFiles = M.insert fp (File (Just h) []) (fileSystemFiles fs)
       }
 
+-- | Closes randomly chosen open file
 genCloseFile :: Gen FileTransition
 genCloseFile = do
-  f <- chooseF
+  -- is executed only if there is at least one open file in the model (and in 'RealWord')
   return $ mkTransition "close file" `goif` anyOpenFile `goto` do
     fs <- get
-    let Just (fp, (File (Just h) _)) = pickOpenFile f fs
+    Just (fp, (File (Just h) _)) <- lift . pick $ pickOpenFile fs
     liftIO $ hClose h
+    -- update the state of the model (closes the file)
     modify $ \fs' -> fs' {
         fileSystemFiles = M.adjust (\(File _ c) -> File Nothing c) fp (fileSystemFiles fs)
       }
 
+-- | Writes a line into randomly chosen open file
 genWriteFile :: Gen FileTransition
 genWriteFile = do
-  f <- chooseF
   l <- listOf (choose ('A', 'z'))
   return $ mkTransition "write line" `goif` anyOpenFile `goto` do
     fs <- get
-    let Just (fp, (File (Just h) _)) = pickOpenFile f fs
-    liftIO $ hPutStrLn h l >> hFlush h
+    Just (fp, (File (Just h) _)) <- lift . pick $ pickOpenFile fs
+    liftIO $ hPutStrLn h l
+    -- update the state of the model (add written line to the end of 'fileContent')
     modify $ \fs' -> fs' {
         fileSystemFiles = M.adjust (\(File h' c) -> File h' (c <> [l])) fp (fileSystemFiles fs)
       }
 
+-- | Reads randomly chosen line from randomly chosen closed file
 genReadFile :: Gen FileTransition
 genReadFile = do
-  f <- chooseF
   return $ mkTransition "read line" `goif` anyClosedFile `goto` do
     fs <- get
-    let
-      Just (fp, (File Nothing ls)) = pickClosedFile f fs
-      lc = L.length ls
+    Just (fp, (File Nothing ls)) <- lift . pick $ pickClosedFile fs
+    let lc = L.length ls
     lift $ do
+      -- additional "precondition" check that the file is not empty
+      -- if it is empty the transition is discarded (no failure) see 'pre'
       pre $ lc > 0
-      let li = f (lc - 1)
+      li <- pick $ choose (0, lc - 1)
+      -- opens file in continuations so it will be closed at the end of "do" block
       fl <- liftIO . (`runContT`return) $ do
         h <- withFileCont fp ReadMode
-        liftIO $ replicateM_ li (hGetLine h)
-        liftIO $ hGetLine h
+        liftIO $ do
+          -- skips all lines before 'li'-st one
+          replicateM_ li (hGetLine h)
+          hGetLine h
+      -- checks that the read line matches corresponding line from the model
       let sl = ls L.!! li
       unless (fl == sl) $ fail (show fl <> " /= " <> show sl)
 
+-- | Invalid reading of file which may fail if the file is empty
+--   (the situation which is not checked here)
 genInvalidReadFile :: Gen FileTransition
 genInvalidReadFile = do
-  f <- chooseF
   return $ mkTransition "invalid read line" `goif` anyClosedFile `goto` do
     fs <- get
-    let Just (fp, _) = pickClosedFile f fs
+    Just (fp, _) <- lift . pick $ pickClosedFile fs
     h <- lift . lift $ withFileCont fp ReadMode
     _ <- liftIO $ hGetLine h
     return ()
 
+-- | Invalid writing to file which does not cause IO exception
+--   but does not update the state of the model (does not add written line to model 'File')
+--   which may lead to assert 'fail' in subsequent transitions
 genInvalidWriteFile :: Gen FileTransition
 genInvalidWriteFile = do
-  f <- chooseF
   l <- listOf (choose ('A', 'z'))
   return $ mkTransition "invalid write line" `goif` anyOpenFile `goto` do
     fs <- get
-    let Just (_, (File (Just h) _)) = pickOpenFile f fs
+    Just (_, (File (Just h) _)) <- lift . pick $ pickOpenFile fs
     liftIO $ hPutStrLn h l
 
-
+-- | Valid transition shall never cause test failure
 prop_success_chain :: Property
 prop_success_chain = monadicCont $ do
   d <- lift $ withSystemTempDirectoryCont "prop_success_chain"
@@ -137,35 +159,33 @@ prop_success_chain = monadicCont $ do
     , (1, genReadFile)
     ]
 
-prop_exception_chain :: Property
-prop_exception_chain = expectFailure . monadicCont $ do
+-- | 'invalid' transitions will eventualy lead to failure
+--   either due to assert 'fail' or IO exception thrown due to invalid file operation
+prop_failure_chain :: Property
+prop_failure_chain = expectFailure . monadicCont $ do
   d <- lift $ withSystemTempDirectoryCont "prop_successful_chain"
   runFSMCont (Environment d) mkFileSystem . listOf1 . frequency $ [
       (10, genCreateFile)
     , (1, genCloseFile)
     , (10, genWriteFile)
     , (1, genReadFile)
+    -- invalid transitions
     , (5, genInvalidReadFile)
     , (5, genInvalidWriteFile)
     ]
 
 
-pickOpenFile :: (Int -> Int) -> FileSystem -> Maybe (FilePath, File)
+pickOpenFile :: FileSystem -> Gen (Maybe (FilePath, File))
 pickOpenFile = pickFile True
 
-pickClosedFile :: (Int -> Int) -> FileSystem -> Maybe (FilePath, File)
+pickClosedFile :: FileSystem -> Gen (Maybe (FilePath, File))
 pickClosedFile = pickFile False
 
-pickFile :: Bool -> (Int -> Int) -> FileSystem -> Maybe (FilePath, File)
-pickFile open sel (FileSystem fs) =
+pickFile :: Bool -> FileSystem -> Gen (Maybe (FilePath, File))
+pickFile open (FileSystem fs) = do
   case [ (fp, f) | (fp, f@(File mh _)) <- M.toList fs, open == isJust mh ] of
-    [] -> Nothing
-    rs -> Just (rs L.!! sel (L.length rs - 1))
-
-chooseF :: Gen (Int -> Int)
-chooseF = do
-  i <- arbitrary
-  return $ \ s -> i `mod` (s + 1)
+    [] -> return Nothing
+    rs -> Just <$> elements rs
 
 anyOpenFile :: FileSystem -> Bool
 anyOpenFile = anyFile True
--- disorder-fsm/test/Test/Disorder/FSM/IO.hs
+++ disorder-fsm/test/Test/Disorder/FSM/IO.hs
@@ -15,6 +15,7 @@ import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 
 
+-- | "External" stateful system "Mutable stack" which is tested here
 newtype Stack a = Stack {
     _toRef :: IORef [a]
   }
@@ -40,15 +41,22 @@ top (Stack r) = readIORef r >>= \case
 size :: Stack a -> IO Int
 size (Stack r) = readIORef r >>= return . length
 
+-- | Environment here is the 'Stack' itself
+type Environment a = Stack a
 
-type StackTransition a = Transition (Stack a) [a] IO
+-- | Model state of the 'Stack' is just a list
+type Model a = [a]
 
+type StackTransition a = Transition (Environment a) (Model a) IO
+
+-- | Pushes random element to stack
 genPush :: Gen (StackTransition Int)
 genPush = do
   a <- arbitrary
   return $ mkTransition ("push " ++ show a) `goto` do
     s <- ask
     liftIO $ push s a
+    -- updates the model
     modify $ (a:)
 
 -- | Broken 'push' which doesn't actually push negative elements
@@ -57,10 +65,12 @@ genInvalidPush = do
   a <- arbitrary
   return $ mkTransition ("invalid_push " ++ show a) `goto` do
     s <- ask
+    -- | this is where it is brocken
     unless (a < 0) $ liftIO $ push s a
+    -- | it does modify the model correctly (so it won't match the 'RealWorld')
     modify $ (a:)
 
-
+-- | Pops an element from 'Stack'
 genPop :: Gen (StackTransition Int)
 genPop =
   return $ mkTransition "pop" `goif` (not . null) `goto` do
@@ -68,35 +78,42 @@ genPop =
     a <- liftIO $ pop s
     l <- get
     lift $ assert (a == head l)
+    -- updates the Model
     modify $ tail
 
--- | Does not specify preCondition so the assertion can fail
+-- | Does not specify 'goif' pre-condition so the assertion can fail
+--   when it is executed with empty 'Stack'
 genInvalidPop :: Gen (StackTransition Int)
 genInvalidPop =
   return $ mkTransition "invalid_pop" `goto` do
     s <- ask
     l <- get
+    -- this may 'fail'
     lift $ assert (not . null $ l)
     a <- liftIO $ pop s
+    -- and this may 'fail' if the model is out of sync with 'RealWorld'
     lift $ assert (a == head l)
     modify $ tail
 
--- | Does not specify preCondition so it will throw exception
+-- | Does not specify 'goif' pre-condition so it may throw exception
 genPopException :: Gen (StackTransition Int)
 genPopException =
   return $ mkTransition "invalid_pop" `goto` do
     s <- ask
     l <- get
+    -- will throw exception on empty 'Stack'
     a <- liftIO $ pop s
     lift $ assert (a == head l)
     modify $ tail
 
+-- | Reads an element on 'Stack' top
 genTop :: Gen (StackTransition Int)
 genTop =
   return $ mkTransition "top" `goto` do
     s <- ask
     ma <- liftIO $ top s
     l <- get
+    -- checks with model
     case (ma, l) of
       (Just a, (x:_)) -> lift $ assert $ a == x
       (Nothing, _) -> lift $ assert $ l == []
@@ -110,21 +127,23 @@ genSize =
     l <- get
     lift . assert $ ss == length l
 
+-- | Shall never fail since only correct transitions are used
 prop_success :: Property
 prop_success = monadicIO $ do
   s <- liftIO empty
-  runFSM s [] (listOf $ oneof [genPush, genPop, genTop, genSize])
+  runFSM s [] . listOf . oneof $ [genPush, genPop, genTop, genSize]
 
+-- | fails due to invalid 'pop'
 prop_pop_assert_error :: Property
 prop_pop_assert_error = expectFailure . monadicIO $ do
   s <- liftIO empty
   runFSM s [] . listOf . oneof $ [genPush, genInvalidPop, genTop, genSize]
 
+-- | fails due to invalid 'push'
 prop_push_assert_error :: Property
 prop_push_assert_error = expectFailure . monadicIO $ do
   s <- liftIO empty
   runFSM s [] . listOf . oneof $ [genInvalidPush, genPush, genPop, genTop, genSize]
-
 
 -- | Produces "invalid" transition less frequently
 --   thus generating longer transition list
@@ -140,6 +159,7 @@ prop_assert_error_longer_chain = expectFailure . monadicIO $ do
     , (10, genSize)
     ]
 
+-- | Fails due to exception with output identical to 'fail' failure
 prop_state_exception :: Property
 prop_state_exception =  expectFailure . monadicIO $ do
   s <- liftIO empty
