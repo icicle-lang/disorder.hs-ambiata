5c6ecc0036f44f6376959ed4309eecda90bb08e1
Amos Robinson <amos.robinson@gmail.com>
2016-08-02 15:23:27 +1000

    Show value diffs for counterexamples

--- disorder-jack/src/Disorder/Jack/Property.hs
+++ disorder-jack/src/Disorder/Jack/Property.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PatternGuards #-}
 module Disorder.Jack.Property (
     gamble
   , gambleRender
@@ -46,23 +47,18 @@ module Disorder.Jack.Property (
   , forAllProperties
   ) where
 
-import           Data.Eq (Eq(..))
 import           Data.Foldable (for_, traverse_)
-import           Data.Function (($), (.))
-import           Data.Functor (Functor(..))
 import qualified Data.List as List
 import           Data.Monoid ((<>))
-import           Data.String (String)
 import           Data.Text (Text)
 import qualified Data.Text as T
 
 import           Disorder.Jack.Core
 import           Disorder.Jack.Tree
 
-import           System.IO (IO, putStrLn)
-
-import           Text.Show (Show)
 import           Text.Show.Pretty (ppShow)
+import qualified Text.Show.Pretty as Pretty
+import           Prelude
 
 import qualified Test.QuickCheck as QC
 import           Test.QuickCheck.All (quickCheckAll, verboseCheckAll, forAllProperties)
@@ -142,4 +138,80 @@ infix 4 ===
 
 (===) :: (Eq a, Show a) => a -> a -> Property
 (===) x y =
-  counterexample (ppShow x <> " /= " <> ppShow y) (x == y)
+  counterexample renderDiff (x == y)
+  where
+    renderDiff
+     | Just x' <- Pretty.reify x
+     , Just y' <- Pretty.reify y
+     = renderDiffs x' y'
+     | otherwise
+     = ppShow x <> " /= " <> ppShow y
+
+-- | Attempt to render difference between two MaxML values
+renderDiffs :: Pretty.Value -> Pretty.Value -> String
+renderDiffs val1 val2 = go 0 val1 val2
+ where
+  go i a b = case (a, b) of
+    _
+     -- If values are same, render normally
+     | a == b
+     -> same i (Pretty.valToStr a)
+     -- If values are different but both fit on one line, just print both
+     | length (List.lines (Pretty.valToStr a)) < 2
+     , length (List.lines (Pretty.valToStr b)) < 2
+     -> ll i a <> rr i b
+
+    -- Both the same constructor with same number of arguments, so check arguments
+    (Pretty.Con m us, Pretty.Con n vs)
+     | n == m
+     , length us == length vs
+     -> same i n <> goes go "" "" "" i (List.zip us vs)
+
+    -- Record constructors, check arguments
+    (Pretty.Rec m us, Pretty.Rec n vs)
+     | n == m
+     , length us == length vs
+     , fmap fst us == fmap fst vs
+     -> same i n
+     -- Print field name too
+     <> goes (\i' (ix,u) (_,v) -> same (i'-1) (ix <> " = ") <> go i' u v) "{" "," "}" i (List.zip us vs)
+
+    -- Tuples and lists of same length
+    (Pretty.Tuple us, Pretty.Tuple vs)
+     | length us == length vs
+     -> goes go "(" "," ")" i (List.zip us vs)
+
+    (Pretty.List us, Pretty.List vs)
+     | length us == length vs
+     -> goes go "[" "," "]" i (List.zip us vs)
+
+    -- Otherwise they are different constructors and we can't descend further, so print both
+    _
+     -> ll i a <> rr i b
+
+  -- Print same
+  same i n = sho " " i n
+  -- Print value on left side
+  ll i v = sho "-" i (Pretty.valToStr v)
+  -- Print value on right side
+  rr i v = sho "+" i (Pretty.valToStr v)
+
+  -- Actually print something
+  sho pre i val
+   = let tabs = List.replicate (i * 2) ' '
+         -- Add the prefix '+' or '-' as well as indentation to each line
+         lns  = List.lines val
+         lns' = fmap (\a -> pre <> tabs <> a) lns
+     in  unlines lns'
+
+  goes gg l m r i uvs
+   = same i l <> goes' False gg m i uvs <> same i r
+
+  goes' putSep gg m i ((u,v):uvs)
+   = let rest = gg (i+1) u v <> goes' True gg m i uvs
+     in if   putSep
+        then same i m <> rest
+        else rest
+  goes' _ _ _ _ []
+   = []
+
